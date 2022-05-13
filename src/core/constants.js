7a32bad1af161c5ea3f63eaddb377e604ccd04f8
Grant Iverson <iversonga@gmail.com>
2022-05-13 18:46:05 -0400

    Added enums for project types

---
+++ src/core/constants.js
@@ -0,0 +1,18 @@
+const constants = {
+    ENUMS: {
+        ProjectTypes: {
+            0: "All",
+            1: "HTML/CSS",
+            2: "JavaScript",
+            3: "Math",
+            4: "React",
+            ALL: 0,
+            "HTML/CSS": 1,
+            JS: 2,
+            MATH: 3,
+            REACT: 4,
+        },
+    },
+};
+
+export default constants;
