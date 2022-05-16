84948019bc55607eb28c97236d86daaae053d9d0
Grant Iverson <iversonga@gmail.com>
2022-05-16 11:30:27 -0400

    Cleaned up App component

--- src/App.js
+++ src/App.js
@@ -6,14 +6,12 @@ import Header from "./components/Header";
 import Main from "./components/Main";
 import Footer from "./components/Footer";
 
-function App() {
-    return (
-        <div className="App">
-            <Header />
-            <Main />
-            <Footer />
-        </div>
-    );
-}
+const App = () => (
+    <div className="App">
+        <Header />
+        <Main />
+        <Footer />
+    </div>
+);
 
 export default App;
