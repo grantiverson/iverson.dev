a67db1168845c714295c6cd534b75cadbae330a3
Grant Iverson <iversonga@gmail.com>
2022-05-17 13:32:31 -0400

    Revert "forcing test to fail"
    This reverts commit 43ccf83eaea8d557252536b8d286be9757279043.

--- src/components/common/NavLinks/NavLink.js
+++ src/components/common/NavLinks/NavLink.js
@@ -10,13 +10,7 @@ const NavLink = ({ active, onClick, text }) => {
 
     return (
         <li className="nav-links__link">
-            <span
-                {...{
-                    className,
-                    // onClick
-                }}
-                role="link"
-            >
+            <span {...{ className, onClick }} role="link">
                 {text}
             </span>
         </li>
