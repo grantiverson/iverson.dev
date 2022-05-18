ea19d9b2ee39c6f0cc8d24c7005a3275998087f0
Grant Iverson <iversonga@gmail.com>
2022-05-18 11:27:33 -0400

    Revert "force format error"
    This reverts commit 7623c3fb9d9fea99375d251af1a5e8962d325018.

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Main from "./components/Main";
 import Footer from "./components/Footer";
 
 const App = () => (
-                                        <div className="App">
+    <div className="App">
         <Header />
         <Main />
         <Footer />
