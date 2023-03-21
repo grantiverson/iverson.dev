5d8e1c19bae8ca901cfa298e476082ed4ed82524
Grant Iverson <iversonga@gmail.com>
2023-03-21 00:36:51 -0400

    move portfolio into package

--- packages/portfolio/src/App.js
+++ packages/portfolio/src/App.js
@@ -1,10 +1,10 @@
 import React from "react";
 
-import "./App.scss";
-
 import Header from "./components/Header";
 import Main from "./components/Main";
 import Footer from "./components/Footer";
+
+import "./App.scss";
 
 const App = () => (
     <div className="App">
