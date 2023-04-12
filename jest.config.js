14fccefea04163d909ffa9322985aa63a4ad4bc0
Grant Iverson <iversonga@gmail.com>
2023-04-12 19:26:54 -0400

    Clean up package configs
    Updated:
    GH workflow
    ESLint
    Git ignore
    Husky
    Prettier
    Jest
    Editorconfig

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,23 @@
 module.exports = {
+    collectCoverage: true,
+    collectCoverageFrom: [
+        "src/**/*.js",
+        "!src/**/index.js",
+        "!src/babel-plugin-macros.config.js",
+    ],
     coverageThreshold: {
         global: {
-            lines: 50,
+            statements: 100,
+            branches: 100,
+            lines: 100,
+            functions: 100,
         },
     },
+    moduleNameMapper: {
+        "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
+            "<rootDir>/__mocks__/fileMock.js",
+        "\\.(css|scss)$": "<rootDir>/__mocks__/styleMock.js",
+    },
+    setupFilesAfterEnv: ["<rootDir>jest-setup.js"],
+    testEnvironment: "jsdom",
 };
