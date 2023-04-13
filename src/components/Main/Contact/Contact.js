87b6a4d3f408be371a5b8c4cc8acb69b52099346
Grant Iverson <iversonga@gmail.com>
2023-04-12 21:36:54 -0400

    Center contacts

--- src/components/Main/Contact/Contact.js
+++ src/components/Main/Contact/Contact.js
@@ -18,53 +18,45 @@ const Contact = () => (
             hire a developer, contact me at the information below.
         </p>
         <div className="contact__links">
-            <div className="contact__icons">
-                <p>
-                    <Envelope />
-                </p>
-                <p>
-                    <Phone />
-                </p>
-                <p>
-                    <LinkedIn />
-                </p>
-                <p>
-                    <GitHub />
-                </p>
-                <p>
-                    <File />
-                </p>
-            </div>
-            <div className="contact__info">
-                <a href="mailto:iversonga@gmail.com">
-                    <p>iversonga@gmail.com</p>
-                </a>
-                <a href="tel:1-704-433-0293">
-                    <p>704.433.0293</p>
-                </a>
+            <p>
+                <Envelope />
+                <a href="mailto:iversonga@gmail.com">iversonga@gmail.com</a>
+            </p>
+            <p>
+                <Phone />
+                <a href="tel:1-704-433-0293">704.433.0293</a>
+            </p>
+            <p>
+                <LinkedIn />
                 <a
                     href="https://www.linkedin.com/pub/grant-iverson/29/247/284"
                     target="_blank"
                     rel="noreferrer"
                 >
-                    <p>LinkedIn</p>
+                    LinkedIn
                 </a>
+            </p>
+            <p>
+                <GitHub />
                 <a
                     href="https://github.com/grantiverson"
                     target="_blank"
                     rel="noreferrer"
                 >
-                    <p>GitHub</p>
+                    GitHub
                 </a>
+            </p>
+            <p>
+                <File />
                 <a
                     href="https://drive.google.com/file/d/10rkFYTW0kT7WOM54zWzGYimWr-g2T3Ak/view?usp=sharing"
                     target="_blank"
                     rel="noreferrer"
                     download
                 >
-                    <p>Resume</p>
+                    Resume
                 </a>
-            </div>
+            </p>
         </div>
     </section>
 );
